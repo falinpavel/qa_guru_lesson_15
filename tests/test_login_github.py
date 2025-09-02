574a2ccbf647f29a13df850edfaa353322c56fda
Falin Pavel <falinpavel96@gmail.com>
2025-09-02 15:59:01 +0300

    add parametrization for run tests

--- tests/test_login_github.py
+++ tests/test_login_github.py
@@ -1,5 +1,14 @@
-from selene import browser
+import pytest
+from selene import browser, be, have
+from selene.core.condition import Condition
 
 
-def test_login_github():
+def test_login_github(request):
     browser.open("https://github.com")
+    if request.config.getoption("--run-mode") == "mobile":
+        browser.element('.js-header-menu-toggle').should(Condition.by_and(be.clickable)).click()
+        browser.element('.HeaderMenu-link--sign-up').should(
+            Condition.by_and(be.clickable, have.text("Sign up"))).click()
+    else:
+        browser.element('.HeaderMenu-link--sign-up').should(Condition.by_and(
+            be.clickable, have.text("Sign up"))).click()
